56b984e69497b0a784358cfeef97b1ef44710c99
maulana2612 <55431615+maulana2612@users.noreply.github.com>
2019-09-23 16:02:20 +0700

    Add files via upload

---
+++ application/controllers/bidang.php
@@ -0,0 +1,28 @@
+<?php
+	Class bidang extends CI_controller {
+
+		function __construct(){
+
+		parent::__construct();
+
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->model('My_model');
+		$this->load->library('session');
+							
+
+
+		}
+		function index (){
+
+			$this->load->view('bidang');
+		}
+
+	}
+
+
+
+
+
+	
+
---
+++ application/controllers/staff.php
@@ -0,0 +1,28 @@
+<?php
+	Class staff extends CI_controller {
+
+		function __construct(){
+
+		parent::__construct();
+
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->model('My_model');
+		$this->load->library('session');
+							
+
+
+		}
+		function index (){
+
+			$this->load->view('Staff');
+		}
+
+	}
+
+
+
+
+
+	
+
